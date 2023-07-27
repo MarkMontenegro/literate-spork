de477521675eeac347ce8fc0dd3fac8a000a2477
MarkMontenegro <markgames1khgu@gmail.com>
2023-07-27 05:42:19 +0200

    Se agrega el Proyecto

---
+++ src/Global/Global.js
@@ -0,0 +1,12 @@
+var Global = {
+	playlistLimit: 50,
+	songLimit: 100,
+	access_token: "",
+	refresh_token: "",
+	redirect_uri : "http://localhost:3000/",
+	client_id : "3a16a11461ea40939e40122f76b97107",
+	client_secret : "d931fc885e144940afff88d3107e5eda",
+	scopes : "user-read-private user-read-email playlist-read-private"
+}
+
+export default Global;
